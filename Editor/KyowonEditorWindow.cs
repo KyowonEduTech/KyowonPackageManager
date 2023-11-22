c93217712222375d7a7ced21488c6573ce44970a
yoonsung <yoonsung@kyowon.co.kr>
2023-11-22 17:17:17 +0900

    [Editor] 인증 화면에 Github Token 발급 가이드 문서 Link 추가

--- Editor/KyowonEditorWindow.cs
+++ Editor/KyowonEditorWindow.cs
@@ -11,9 +11,12 @@ namespace KyowonPackageManager.Editor
         private const string EDITOR_CERTIFICATION_WINDOW_TITLE = "Kyowon Certification Windnow";
         private const string EDITOR_DOWNLOAD_WINDOW_TITLE = "Kyowon Package Download Windnow";
 
-        private const string GITHUB_TOKEN_GUIDE = "Kyowon GitHub 인증키가 필요합니다.\n\n" +
+        private const string GITHUB_TOKEN_GUIDE = "\nKyowon GitHub 인증키가 필요합니다.\n\n" +
                                           "1. Project 담당자에게 GitHub KyowonEduTech Repository 의 권한을 요청하세요.\n" +
-                                          "2. GitHub 개인 계정에서 Personal Access Token 을 발급받아 입력하세요.\n";
+                                          "2. GitHub 개인 계정에서 Personal Access Token 을 발급받아 입력하세요.\n" +
+                                          "3. 정상 인증 시, [Macintosh HD] - [사용자] - [유저이름] - .upmconfig.toml 파일이 생성됩니다.\n";
+        private const string GITHUB_TOKEN_GUIDE_URL = "https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens";
+
         private const string GITHUB_PACKAGE_DOWNLOAD_GUIDE = "Kyowon Project Manager 를 다운로드하세요.";
 
         private const string KYOWON_PACKAGE_DOCUMENT_URL = "https://docs.google.com/document/d/1VA3VgsjUbBkwESblH3JFVOWyZQ6IfyG0ZwpgHqvPqcU/edit?usp=sharing";
@@ -63,10 +66,22 @@ namespace KyowonPackageManager.Editor
         {
             if (!KyowonCertificationManager.HasPackagePermission())
             {
-                Window.maxSize = new Vector2(500, 115);
-                Window.minSize = new Vector2(500, 115);
+                Window.maxSize = new Vector2(520, 160);
+                Window.minSize = new Vector2(520, 160);
 
                 EditorGUILayout.HelpBox(GITHUB_TOKEN_GUIDE, MessageType.Warning);
+
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+
+                GUIStyle rightAlignedStyle = new GUIStyle(EditorStyles.miniButton);
+                rightAlignedStyle.alignment = TextAnchor.MiddleRight;
+
+                if (EditorGUILayout.LinkButton("GitHub Token 발급 가이드 Link"))
+                {
+                    Application.OpenURL(GITHUB_TOKEN_GUIDE_URL);
+                }
+                GUILayout.EndHorizontal();
 
                 _inputKey = EditorGUILayout.TextField("Personal Access Token", _inputKey);
                 if (GUILayout.Button("Certification"))
